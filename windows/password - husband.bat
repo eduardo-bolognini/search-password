854335b580e8f439e18ba9584376943c523a8055
eduardo-bolognini <76057233+eduardo-bolognini@users.noreply.github.com>
2021-08-17 10:22:16 +0200

    Add files via upload

---
+++ windows/password - husband.bat
@@ -0,0 +1,277 @@
+echo data
+PAUSE
+echo name
+set  /p "name=>"
+echo name (M)
+set  /p "name_M=>"
+echo nickname
+set /p "nickname=>"
+echo nickname (M)
+set /p "nickname_M=>"
+echo last name
+set /p "lastname=>"
+echo last name (M)
+set /p "lastname_M=>"
+echo small last name
+set /p "small_lastname=>"
+echo small last name (M)
+set /p "small_lastname_M=>"
+echo day of birth
+set /p "day_of_birth=>"
+echo month of birth
+set /p "month_of_birth=>"
+echo year of birth
+set /p  "year_of_birth=>"
+echo small year of birth
+set /p "small_year_of_birth=>"
+
+echo data husband/wife
+
+
+
+echo name husband/wife
+set  /p "name_husband/wife=>"
+echo name husband/wife (M)
+set  /p "name_husband/wife_M=>"
+echo nickname husband/wife
+set /p "nickname_husband/wife=>"
+echo nickname husband/wife (M)
+set /p "nickname_husband/wife_M=>"
+echo last name husband/wife
+set /p "lastname_husband/wife=>"
+echo last name husband/wife (M)
+set /p "lastname_husband/wife_M=>"
+echo small last name husband/wife
+set /p "small_lastname_husband/wife=>"
+echo small last name husband/wife (M)
+set /p "small_lastname_husband/wife_M=>"
+echo day of birth husband/wife
+set /p "day_of_birth_husband/wife=>"
+echo month of birth husband/wife
+set /p "month_of_birth_husband/wife=>"
+echo year of birth husband/wife
+set /p  "year_of_birth_husband/wife=>"
+echo small year of birth husband/wife
+set /p "small_year_of_birth_husband/wife=>" 
+
+echo data both of them
+
+echo anniversary day
+set /p "day_anniversary=>"
+echo anniversary day
+set /p "month_anniversary=>"
+echo anniversary day
+set /p "year_anniversary=>"
+echo anniversary small year
+set /p "small_year_anniversary=>"
+
+:password
+
+echo password
+
+PAUSE
+
+echo %nickname%%year_of_birth%
+echo %nickname%%small_lastname%%small_year_of_birth%
+echo %name%%lastname%%year_of_birth%
+echo %nickname%_%day_of_birth%%month_of_birth%%year_of_birth%
+echo %nikname%%lastname%%year_of_birth%
+echo %nickname%%day_of_birth%%month_of_birth%%year_of_birth%
+echo %day_of_birth%%month_of_birth%%year_of_birth%
+echo %name%%lastname%%small_year_of_birth%
+
+
+echo %nickname_M%%year_of_birth%
+echo %nickname_M%%small_lastname_M%%small_year_of_birth%
+echo %name_M%%lastname_M%%year_of_birth%
+echo %nickname_M%_%day_of_birth%%month_of_birth%%year_of_birth%
+echo %nikname_M%%lastname_M%%year_of_birth%
+echo %nickname_M%%day_of_birth%%month_of_birth%%year_of_birth%
+echo %name_M%%lastname_M%%small_year_of_birth%
+
+
+echo %nickname_M%%year_of_birth%
+echo %nickname_M%%small_lastname%%small_year_of_birth%
+echo %name_M%%lastname%%year_of_birth%
+echo %nickname_M%_%day_of_birth%%month_of_birth%%year_of_birth%
+echo %nikname_M%%lastname%%year_of_birth%
+echo %nickname_M%%day_of_birth%%month_of_birth%%year_of_birth%
+echo %name_M%%lastname%%small_year_of_birth%
+echo %name%%lastname_M%%small_year_of_birth%
+
+
+
+
+echo %nickname_husband/wife%%year_of_birth%
+echo %nickname_husband/wife%%small_lastname_husband/wife%%small_year_of_birth_husband/wife%
+echo %name_husband/wife%%lastname_husband/wife%%year_of_birth_husband/wife%
+echo %nickname_husband/wife%_%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth%_husband/wife
+echo %nikname_husband/wife%%lastname_husband/wife%%year_of_birth_husband/wife%
+echo %nickname_husband/wife%%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife%
+echo %day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife%
+echo %name%%lastname%%small_year_of_birth%
+
+
+echo %nickname_husband/wife_M%%year_of_birth_husband/wife%
+echo %nickname_husband/wife_M%%small_lastname_husband/wife_M%%small_year_of_birth_husband/wife%
+echo %name_husband/wife_M%%lastname_husband/wife_M%%year_of_birth_husband/wife%
+echo %nickname_husband/wife_M%_%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife%
+echo %nikname_husband/wife_M%%lastname_husband/wife_M%%year_of_birth_husband/wife%
+echo %nickname_husband/wife_M%%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife%
+echo %name_husband/wife_M%%lastname_husband/wife_M%%small_year_of_birth_husband/wife%
+
+
+echo %nickname_husband/wife_M%%year_of_birth_husband/wife%
+echo %nickname_husband/wife_M%%small_lastname_husband/wife%%small_year_of_birth_husband/wife%
+echo %name_husband/wife_M%%lastname_husband/wife%%year_of_birth_husband/wife%
+echo %nickname_husband/wife_M%_%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife%
+echo %nikname_husband/wife_M%%lastname_husband/wife%%year_of_birth_husband/wife%
+echo %nickname_husband/wife_M%%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife%
+echo %name_husband/wife_M%%lastname_husband/wife%%small_year_of_birth_husband/wife%
+echo %name_husband/wife%%lastname_husband/wife_M%%small_year_of_birth_husband/wife%s
+
+echo %name%%name_husband/wife%%anniversary%
+echo %name%%lastname_husband/wife%%small_year_of_birth%
+echo %nickname%%lastname_husband/wife%%small_year_of_birth%
+echo %name_husband/wife%%lastname_husband/wife%small_yeaf_of_birth%
+echo %day_anniversary%%month_anniversary%%year_anniversary%
+echo %day_anniversary%%month_anniversary%%small_year_anniversary%
+echo %name%%lastname%%small_year_anniversary%
+echo %name%%lastname%small_yeaf_of_birth_husband/wife%
+
+echo %name_M%%name_husband/wife_M%%anniversary%
+echo %name_M%%lastname_husband/wife_M%%small_year_of_birth%
+echo %nickname_M%%lastname_husband/wife_M%%small_year_of_birth%
+echo %name_husband/wife_M%%lastname_husband/wife_M%small_yeaf_of_birth%
+echo %name_M%%lastname_M%%small_year_anniversary%
+echo %name_M%%lastname_M%small_yeaf_of_birth_husband/wife%
+
+echo %name_M%%name_husband/wife_M%%anniversary%
+echo %name_M%%name_husband/wife%%anniversary%
+echo %name%%name_husband/wife_M%%anniversary%
+echo %name_M%%lastname_husband/wife%%small_year_of_birth%
+echo %nickname%%lastname_husband/wife_M%%small_year_of_birth%
+echo %nickname_M%%lastname_husband/wife_M%%small_year_of_birth%
+echo %name_husband/wife_M%%lastname_husband/wife%small_yeaf_of_birth%
+echo %name_husband/wife%%lastname_husband/wife_M%small_yeaf_of_birth%
+echo %name_M%%lastname%%small_year_anniversary%
+echo %name%%lastname_M%%small_year_anniversary%
+echo %name_M%%lastname%small_yeaf_of_birth_husband/wife%
+echo %name%%lastname_M%small_yeaf_of_birth_husband/wife%
+
+
+echo save password in the file
+PAUSE
+echo / >> desktop\password.txt
+echo data: >> desktop\password.txt
+echo %name_M% %nickname_M% %lastname_M% %small_lastname_M% %day_of_birth% %month_of_birth% %year_of_birth%  %small_year_of_birth% >> desktop\password.txt
+echo / >> desktop\password.txt
+
+echo %nickname%%year_of_birth% >> desktop\password.txt
+echo %nickname%%small_lastname%%small_year_of_birth% >> desktop\password.txt
+echo %name%%lastname%%year_of_birth% >> desktop\password.txt
+echo %nickname%_%day_of_birth%%month_of_birth%%year_of_birth% >> desktop\password.txt
+echo %nikname%%lastname%%year_of_birth% >> desktop\password.txt
+echo %nickname%%day_of_birth%%month_of_birth%%year_of_birth% >> desktop\password.txt
+echo %day_of_birth%%month_of_birth%%year_of_birth% >> desktop\password.txt
+echo %name%%lastname%%small_year_of_birth% >> desktop\password.txt
+
+
+echo %nickname_M%%year_of_birth% >> desktop\password.txt
+echo %nickname_M%%small_lastname_M%%small_year_of_birth% >> desktop\password.txt
+echo %name_M%%lastname_M%%year_of_birth% >> desktop\password.txt
+echo %nickname_M%_%day_of_birth%%month_of_birth%%year_of_birth% >> desktop\password.txt
+echo %nikname_M%%lastname_M%%year_of_birth% >> desktop\password.txt
+echo %nickname_M%%day_of_birth%%month_of_birth%%year_of_birth% >> desktop\password.txt
+echo %name_M%%lastname_M%%small_year_of_birth% >> desktop\password.txt
+
+
+
+echo %nickname_M%%year_of_birth% >> desktop\password.txt
+echo %nickname_M%%small_lastname%%small_year_of_birth% >> desktop\password.txt
+echo %name_M%%lastname%%year_of_birth% >> desktop\password.txt
+echo %nickname_M%_%day_of_birth%%month_of_birth%%year_of_birth% >> desktop\password.txt
+echo %nikname_M%%lastname%%year_of_birth% >> desktop\password.txt
+echo %nickname_M%%day_of_birth%%month_of_birth%%year_of_birth%  >> desktop\password.txt
+echo %name_M%%lastname%%small_year_of_birth% >> desktop\password.txt
+echo %name%%lastname_M%%small_year_of_birth% >> desktop\password.txt
+
+
+
+echo / >> desktop\password.txt
+echo data husband/wife >> desktop\password.txt
+echo %name_husband/wife_M% %nickname_husband/wife_M% %lastname_husband/wife_M% %small_lastname_husband/wife_M% %day_of_birth_husband/wife% %month_of_birth_husband/wife% %year_of_birth_husband/wife%  %small_year_of_birth_husband/wife% >> desktop\password.txt
+echo / >> desktop\password.txt
+
+echo %nickname_husband/wife%%year_of_birth% >> desktop\password.txt
+echo %nickname_husband/wife%%small_lastname_husband/wife%%small_year_of_birth_husband/wife% >> desktop\password.txt
+echo %name_husband/wife%%lastname_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nickname_husband/wife%_%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth%_husband/wife >> desktop\password.txt
+echo %nikname_husband/wife%%lastname_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nickname_husband/wife%%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %name%%lastname%%small_year_of_birth% >> desktop\password.txt
+
+
+echo %nickname_husband/wife_M%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nickname_husband/wife_M%%small_lastname_husband/wife_M%%small_year_of_birth_husband/wife% >> desktop\password.txt
+echo %name_husband/wife_M%%lastname_husband/wife_M%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nickname_husband/wife_M%_%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nikname_husband/wife_M%%lastname_husband/wife_M%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nickname_husband/wife_M%%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %name_husband/wife_M%%lastname_husband/wife_M%%small_year_of_birth_husband/wife%>> desktop\password.txt
+ 
+
+echo %nickname_husband/wife_M%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nickname_husband/wife_M%%small_lastname_husband/wife%%small_year_of_birth_husband/wife% >> desktop\password.txt
+echo %name_husband/wife_M%%lastname_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nickname_husband/wife_M%_%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nikname_husband/wife_M%%lastname_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %nickname_husband/wife_M%%day_of_birth_husband/wife%%month_of_birth_husband/wife%%year_of_birth_husband/wife% >> desktop\password.txt
+echo %name_husband/wife_M%%lastname_husband/wife%%small_year_of_birth_husband/wife% >> desktop\password.txt
+echo %name_husband/wife%%lastname_husband/wife_M%%small_year_of_birth_husband/wife% >> desktop\password.txt
+
+
+echo / >> desktop\password.txt
+echo data both of them >> desktop\password.txt
+echo %day_>> desktop\password.txt
+echo / >> desktop\password.txt
+
+echo %name%%name_husband/wife%%day_anniversary%%month_anniversary%year_anniversary% >> desktop\password.txt
+echo %name%%name_husband/wife%small_year_anniversary%  >> desktop\password.txt
+echo %name%%name_husband/wife%%day_anniversary%%month_anniversary%year_anniversary% >> desktop\password.txt
+echo %name%%lastname_husband/wife%%small_year_of_birth% >> desktop\password.txt
+echo %nickname%%lastname_husband/wife%%small_year_of_birth% >> desktop\password.txt
+echo %name_husband/wife%%lastname_husband/wife%small_yeaf_of_birth% >> desktop\password.txt
+echo %day_anniversary%%month_anniversary%%year_anniversary% >> desktop\password.txt
+echo %day_anniversary%%month_anniversary%%small_year_anniversary% >> desktop\password.txt
+echo %name%%lastname%%small_year_anniversary% >> desktop\password.txt
+echo %name%%lastname%small_yeaf_of_birth_husband/wife% >> desktop\password.txt
+
+echo %name_M%%name_husband/wife_M%%day_anniversary%%month_anniversary%year_anniversary% >> desktop\password.txt
+echo %name_M%%lastname_husband/wife_M%%small_year_of_birth% >> desktop\password.txt
+echo %nickname_M%%lastname_husband/wife_M%%small_year_of_birth% >> desktop\password.txt
+echo %name_husband/wife_M%%lastname_husband/wife_M%small_yeaf_of_birth% >> desktop\password.txt
+echo %name_M%%lastname_M%%small_year_anniversary% >> desktop\password.txt
+echo %name_M%%lastname_M%small_yeaf_of_birth_husband/wife% >> desktop\password.txt
+
+echo %name_M%%name_husband/wife_M%%day_anniversary%%month_anniversary%year_anniversary% >> desktop\password.txt
+echo %name_M%%name_husband/wife%%day_anniversary%%month_anniversary%year_anniversary% >> desktop\password.txt
+echo %name%%name_husband/wife_M%%day_anniversary%%month_anniversary%year_anniversary% >> desktop\password.txt
+echo %name_M%%name_husband/wife_M%%small_year_anniversary% >> desktop\password.txt
+echo %name_M%%name_husband/wife%%small_year_anniversary% >> desktop\password.txt
+echo %name%%name_husband/wife_M%%small_year_anniversary% >> desktop\password.txt
+echo %name_M%%lastname_husband/wife%%small_year_of_birth% >> desktop\password.txt
+echo %nickname%%lastname_husband/wife_M%%small_year_of_birth% >> desktop\password.txt
+echo %nickname_M%%lastname_husband/wife_M%%small_year_of_birth% >> desktop\password.txt
+echo %name_husband/wife_M%%lastname_husband/wife%small_yeaf_of_birth% >> desktop\password.txt
+echo %name_husband/wife%%lastname_husband/wife_M%small_yeaf_of_birth% >> desktop\password.txt
+echo %name_M%%lastname%%small_year_anniversary% >> desktop\password.txt
+echo %name%%lastname_M%%small_year_anniversary% >> desktop\password.txt
+echo %name_M%%lastname%small_yeaf_of_birth_husband/wife% >> desktop\password.txt
+echo %name%%lastname_M%small_yeaf_of_birth_husband/wife% >> desktop\password.txt
+
+
+
+
+
